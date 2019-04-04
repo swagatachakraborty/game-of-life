e161f3c8de672c62bf7079a8ddce4dee7775a269
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-04-04 18:02:38 +0530

    Updates

---
+++ precache-manifest.6dffeb0f86f7fc2560882c2412a52bd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9d05e43578a5ddb80d1",
+    "url": "/game-of-life/static/js/runtime~main.dfaa7c2d.js"
+  },
+  {
+    "revision": "8e9ec6428ab610ee726d",
+    "url": "/game-of-life/static/js/main.289ad04a.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/game-of-life/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "8e9ec6428ab610ee726d",
+    "url": "/game-of-life/static/css/main.345686a7.chunk.css"
+  },
+  {
+    "revision": "088b6864c811f92a55446b6bca9b0b72",
+    "url": "/game-of-life/index.html"
+  }
+];
